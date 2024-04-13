712b0577631db579f52b5122958770ffa3efe3d5
hoidq9 <daoquochoi06092002@gmail.com>
2024-04-14 00:48:20 +0700

    change Dark to Light

--- themes/themes.sh
+++ themes/themes.sh
@@ -10,8 +10,8 @@ themes() {
 	cd WhiteSur-gtk-theme
 	# vi -u NONE -c 'g/&#panelActivities/normal! d%' -c 'wq' src/sass/gnome-shell/common/_panel.scss
 	gawk -i inplace '!/Yaru/' src/main/gnome-shell/gnome-shell-theme.gresource.xml
-	sudo ./install.sh -n 'rhel' -o normal -c Dark -a alt -t default -p 60 -P smaller -s default -b default -m -N mojave -HD --normal --round --right -i standard
-	sudo ./tweaks.sh -g default -o normal -c Dark -t default -p 60 -P smaller -n -i standard -b default
+	sudo ./install.sh -n 'rhel' -o normal -c Light -a alt -t default -p 60 -P smaller -s default -b default -m -N mojave -HD --normal --round --right -i standard
+	sudo ./tweaks.sh -g default -o normal -c Light -t default -p 60 -P smaller -n -i standard -b default
 }
 
 themes &>$HOME/Drive/logs/themes.log
