4ea6ba1b731e44873c608480b3bbcb42fe946d12
hoidq9 <hoidq9@gmail.com>
2024-10-29 23:59:35 +0700

    update

--- fedora_setup/system/conky/cpu_power.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while true; do
-    time=1
-    sum_1=$(cat /sys/class/powercap/*/energy_uj | awk 'BEGIN { sum = 0; } { sum += $1; } END { print sum; }')
-    sleep $time
-    sum_2=$(cat /sys/class/powercap/*/energy_uj | awk 'BEGIN { sum = 0; } { sum += $1; } END { print sum; }')
-    res=$(echo "scale=1; (($sum_2 - $sum_1) / 1000000) / $time" | bc -l)
-    echo "$res" >$_HOME/Prj/conky/cpu_power.txt
-done
