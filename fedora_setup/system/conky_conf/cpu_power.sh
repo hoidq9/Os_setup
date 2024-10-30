78b03e21b667235f2f022dba902be0b1d7945503
hoidq9 <hoidq9@gmail.com>
2024-10-30 23:08:04 +0700

    update

--- fedora_setup/system/conky_conf/cpu_power.sh
+++ fedora_setup/system/conky_conf/cpu_power.sh
@@ -6,4 +6,5 @@ while true; do
     sum_2=$(cat /sys/class/powercap/intel-rapl:0/energy_uj | awk 'BEGIN { sum = 0; } { sum += $1; } END { print sum; }')
     res=$(echo "scale=1; (($sum_2 - $sum_1) / 1000000) / $time" | bc -l)
     echo "$res" >/cpuH/cpu_power.txt
+    echo $(dmidecode -t memory | grep "Manufacturer:" | sort | uniq) >/cpuH/ram_manufacturer.txt
 done
