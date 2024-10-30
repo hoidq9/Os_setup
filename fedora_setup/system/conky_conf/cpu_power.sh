a0eefbc58532909365dd30a531ee950ee799a38d
hoidq9 <hoidq9@gmail.com>
2024-10-30 21:38:16 +0700

    update

--- fedora_setup/system/conky_conf/cpu_power.sh
+++ fedora_setup/system/conky_conf/cpu_power.sh
@@ -5,5 +5,5 @@ while true; do
     sleep $time
     sum_2=$(cat /sys/class/powercap/intel-rapl:0/energy_uj | awk 'BEGIN { sum = 0; } { sum += $1; } END { print sum; }')
     res=$(echo "scale=1; (($sum_2 - $sum_1) / 1000000) / $time" | bc -l)
-    echo "$res" >$_HOME/Prj/conky_conf/cpu_power.txt
+    echo "$res" >/cpuH/cpu_power.txt
 done
--- fedora_setup/system/conky_conf/cpu_voltage.sh
+++ fedora_setup/system/conky_conf/cpu_voltage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true; do
-    echo $(sudo rdmsr 0x198 -u --bitfield 47:32 | awk '{printf "%.1f", $1/8192}') >$_HOME/Prj/conky_conf/cpu_voltage.txt
+    echo $(sudo rdmsr 0x198 -u --bitfield 47:32 | awk '{printf "%.1f", $1/8192}') >/cpuH/cpu_voltage.txt
     sleep 0.3
 done
