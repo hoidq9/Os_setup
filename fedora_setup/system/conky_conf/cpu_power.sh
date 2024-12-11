beb1dc0613abe69f9aed117b54d59153c993f55a
hoidq9 <hoidq9@gmail.com>
2024-12-12 01:23:38 +0700

    update

--- fedora_setup/system/conky_conf/cpu_power.sh
+++ fedora_setup/system/conky_conf/cpu_power.sh
@@ -6,11 +6,11 @@ while true; do
     sum_2=$(cat /sys/class/powercap/intel-rapl:0/energy_uj | awk 'BEGIN { sum = 0; } { sum += $1; } END { print sum; }')
     res=$(echo "scale=1; (($sum_2 - $sum_1) / 1000000) / $time" | bc -l)
     if (( $(echo "$res < 10" | bc -l) )); then
-        printf "%.3f\n" "$res" >/cpuH/cpu_power.txt
+        printf "%.3f\n" "$res" >/Os_H/cpu_power.txt
     elif (( $(echo "$res >= 10 && $res < 100" | bc -l) )); then
-        printf "%.2f\n" "$res" >/cpuH/cpu_power.txt
+        printf "%.2f\n" "$res" >/Os_H/cpu_power.txt
     else
-        printf "%.1f\n" "$res" >/cpuH/cpu_power.txt
+        printf "%.1f\n" "$res" >/Os_H/cpu_power.txt
     fi
-    echo $(dmidecode -t memory | grep "Manufacturer:" | sort | uniq | awk '{print $2" "$3}') >/cpuH/ram_manufacturer.txt
+    echo $(dmidecode -t memory | grep "Manufacturer:" | sort | uniq | awk '{print $2" "$3}') >/Os_H/ram_manufacturer.txt
 done
--- fedora_setup/system/conky_conf/cpu_voltage.sh
+++ fedora_setup/system/conky_conf/cpu_voltage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true; do
-    echo $(sudo rdmsr 0x198 -u --bitfield 47:32 | awk '{printf "%.1f", $1/8192}') >/cpuH/cpu_voltage.txt
+    echo $(sudo rdmsr 0x198 -u --bitfield 47:32 | awk '{printf "%.1f", $1/8192}') >/Os_H/cpu_voltage.txt
     sleep 0.3
 done
