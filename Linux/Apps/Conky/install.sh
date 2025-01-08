308fa1df5b5ca3cbf6bb209aa64e6223f970a9f0
hoidq9 <hoidq9@gmail.com>
2025-01-09 00:55:35 +0700

    update

--- Linux/Apps/Conky/install.sh
+++ Linux/Apps/Conky/install.sh
@@ -3,17 +3,18 @@ source ../../variables.sh
 
 Apps_Conky() {
     cd $REPO_DIR
-    if [ "$os_id" == "fedora" ]; then
-        cd fedora
-        if rpm -q conky; then
 
-            if [ ! -d /home/$user_current/.config/autostart ]; then
-                mkdir -p /home/$user_current/.config/autostart
-            fi
+    if rpm -q conky; then
 
-            cp conky.desktop /home/$user_current/.config/autostart
-            mkdir -p /home/$user_current/.config/conky
+        if [ ! -d /home/$user_current/.config/autostart ]; then
+            mkdir -p /home/$user_current/.config/autostart
+        fi
 
+        cp conky.desktop /home/$user_current/.config/autostart
+        mkdir -p /home/$user_current/.config/conky
+
+        if [ "$os_id" == "fedora" ]; then
+            cd fedora
             if loginctl show-session $(loginctl list-sessions | grep $user_current | awk '{print $1}') -p Type | grep -q "wayland"; then
                 cp conky_wayland.conf ~/.config/conky/
                 mv ~/.config/conky/conky_wayland.conf ~/.config/conky/conky.conf
@@ -21,9 +22,14 @@ Apps_Conky() {
                 cp conky_x11.conf ~/.config/conky/
                 mv ~/.config/conky/conky_x11.conf ~/.config/conky/conky.conf
             fi
+
+        elif [ "$os_id" == "rhel" ]; then
+            cd rhel
+            if [ ! -f /home/$user_current/.config/conky/conky.conf ]; then
+                cp conky.conf /home/$user_current/.config/conky/
+            fi
         fi
-    else
-        echo "The script is not supported on this OS."
     fi
 }
+
 check_and_run Apps_Conky "$REPO_DIR/../../logs/Apps_Conky.log" "$REPO_DIR/../../logs/Result.log"
