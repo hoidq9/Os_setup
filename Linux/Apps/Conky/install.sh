74ba7421ff0e62b3ccabac64d97bfa43bee1a164
hoidq9 <hoidq9@gmail.com>
2025-01-16 10:59:11 +0700

    update

--- Linux/Apps/Conky/install.sh
+++ Linux/Apps/Conky/install.sh
@@ -23,17 +23,9 @@ Apps_Conky() {
                 mv ~/.config/conky/conky_x11.conf ~/.config/conky/conky.conf
             fi
 
-        elif [ "$os_id" == "rhel" ]; then
+        elif [ "$os_id" == "rhel" ] || [ "$os_id" == "almalinux" ]; then
             cd rhel
-            if [ ! -f /home/$user_current/.config/conky/conky.conf ]; then
-                cp conky.conf /home/$user_current/.config/conky/
-            fi
-            
-        elif [ "$os_id" == "almalinux" ]; then
-            cd rhel
-            if [ ! -f /home/$user_current/.config/conky/conky.conf ]; then
-                cp conky.conf /home/$user_current/.config/conky/
-            fi
+            cp -f conky.conf /home/$user_current/.config/conky/
         fi
     fi
 }
