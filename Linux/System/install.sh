2efce6dac986374a3e8772ee6483cc5f6177b300
hoidq9 <hoidq9@gmail.com>
2025-05-31 17:17:52 +0700

    update

--- Linux/System/install.sh
+++ Linux/System/install.sh
@@ -141,7 +141,7 @@ rhel_system() {
 	}
 
 	packages() {
-		dnf install zsh gnome-shell gnome-browser-connector ptyxis nautilus PackageKit-command-not-found gnome-software gdm git dbus-x11 ibus-m17n podman msr-tools redhat-mono-fonts gnome-disk-utility rhc rhc-worker-playbook gdb gcc seahorse ansible-core yara gnome-system-monitor gnome-tweaks -y # dconf-editor gnome-extensions-app.x86_64 cockpit-machines virt-manager yandex-browser-stable gnome-terminal gnome-terminal-nautilus chrome-gnome-shell podman-compose conky cockpit-podman
+		dnf install zsh gnome-shell gnome-browser-connector ptyxis nautilus PackageKit-command-not-found gnome-software gdm git dbus-x11 ibus-m17n podman msr-tools redhat-mono-fonts gnome-disk-utility rhc rhc-worker-playbook gdb gcc seahorse ansible-core yara gnome-system-monitor gnome-tweaks cockpit-machines virt-manager cockpit-podman cockpit -y # dconf-editor gnome-extensions-app.x86_64 yandex-browser-stable gnome-terminal gnome-terminal-nautilus chrome-gnome-shell podman-compose conky 
 		dnf group install "Fonts" -y
 		# systemctl restart libvirtd
 	}
