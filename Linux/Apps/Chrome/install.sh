7b573d63dc80c64a49a388902669399de9e08e97
hoidq9 <hoidq9@gmail.com>
2025-06-01 17:24:04 +0700

    update

--- Linux/Apps/Chrome/install.sh
+++ Linux/Apps/Chrome/install.sh
@@ -5,6 +5,6 @@ Apps_Chrome() {
     flatpak install flathub com.google.Chrome -y
 }
 
-if [ "$os_id" == "rhel" ]; then
-    check_and_run Apps_Chrome "$REPO_DIR/../../logs/Apps_Chrome.log" "$REPO_DIR/../../logs/Result.log"
-fi
+# if [ "$os_id" == "rhel" ]; then
+#     check_and_run Apps_Chrome "$REPO_DIR/../../logs/Apps_Chrome.log" "$REPO_DIR/../../logs/Result.log"
+# fi
