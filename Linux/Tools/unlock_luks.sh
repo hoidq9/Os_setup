bb68f365c967d89785cba8cfbab56d1f882b25d6
hoidq9 <hoidq9@gmail.com>
2025-10-09 15:35:51 +0700

    update method unlock luks2 disk with tpm2 and fido2

--- Linux/Tools/unlock_luks.sh
+++ Linux/Tools/unlock_luks.sh
@@ -10,8 +10,8 @@ NC='\033[0m' # Không màu
 
 # Kiểm tra quyền root
 if [ "$EUID" -ne 0 ]; then
-    echo -e "${RED}This script must be run as root.${NC}"
-    exit 1
+	echo -e "${RED}This script must be run as root.${NC}"
+	exit 1
 fi
 
 echo
@@ -21,20 +21,20 @@ encrypted_partitions=()
 echo -e "${BLUE}--- Scanning for LUKS encrypted partitions... ---${NC}"
 
 while read -r line; do
-    # Trích xuất đường dẫn thiết bị từ đầu dòng
-    partition=$(echo "$line" | awk -F: '{print $1}')
-    encrypted_partitions+=("$partition")
+	# Trích xuất đường dẫn thiết bị từ đầu dòng
+	partition=$(echo "$line" | awk -F: '{print $1}')
+	encrypted_partitions+=("$partition")
 done < <(blkid | grep "crypto_LUKS")
 
 # Hiển thị kết quả
 if [[ ${#encrypted_partitions[@]} -gt 0 ]]; then
-    echo -e "${GREEN}Các phân vùng được mã hóa LUKS:${NC}"
-    for partition in "${encrypted_partitions[@]}"; do
-        echo -e "${YELLOW}  - $partition${NC}"
-    done
+	echo -e "${GREEN}Các phân vùng được mã hóa LUKS:${NC}"
+	for partition in "${encrypted_partitions[@]}"; do
+		echo -e "${YELLOW}  - $partition${NC}"
+	done
 else
-    echo -e "${RED}Không tìm thấy phân vùng mã hóa LUKS.${NC}"
-    exit 0
+	echo -e "${RED}Không tìm thấy phân vùng mã hóa LUKS.${NC}"
+	exit 0
 fi
 
 # Thêm một dòng trống để cách biệt
@@ -54,18 +54,18 @@ read -r enroll_fido2
 
 if [[ "$enroll_fido2" == "y" ]]; then
 
-    # if [ "$os_id" == "rhel" ]; then
-    bash $REPO_DIR/edit_crypttab.sh
-    # fi
+	# if [ "$os_id" == "rhel" ]; then
+	bash $REPO_DIR/edit_crypttab.sh
+	# fi
 
-    echo -ne "${YELLOW}Type the path to the FIDO2 device (Enter correctly, not contain spaces) (ex: /dev/hidraw?): ${NC}"
-    read -r fido2_device_path
-    echo -ne "${YELLOW}Type the disk path of the LUKS2 partition (Enter correctly, not contain spaces) (ex: /dev/nvme0n1p?): ${NC}"
-    read -r luks2_disk_path
-    # echo "add_dracutmodules+=\" fido2 \"" | sudo tee /etc/dracut.conf.d/fido2.conf
-    systemd-cryptenroll --fido2-device=$fido2_device_path --fido2-with-client-pin=no --fido2-with-user-verification=yes --fido2-with-user-presence=yes $luks2_disk_path
-    dracut -f
-    echo -e "${GREEN}FIDO2 device enrolled successfully.${NC}"
+	echo -ne "${YELLOW}Type the path to the FIDO2 device (Enter correctly, not contain spaces) (ex: /dev/hidraw?): ${NC}"
+	read -r fido2_device_path
+	echo -ne "${YELLOW}Type the disk path of the LUKS2 partition (Enter correctly, not contain spaces) (ex: /dev/nvme0n1p?): ${NC}"
+	read -r luks2_disk_path
+	# echo "add_dracutmodules+=\" fido2 \"" | sudo tee /etc/dracut.conf.d/fido2.conf
+	systemd-cryptenroll --fido2-device=$fido2_device_path --fido2-with-client-pin=yes --fido2-with-user-verification=yes --fido2-with-user-presence=yes $luks2_disk_path
+	dracut -f
+	echo -e "${GREEN}FIDO2 device enrolled successfully.${NC}"
 fi
 
 echo
@@ -74,19 +74,19 @@ echo -ne "${YELLOW}Do you want to enroll a new TPM2 device to unlock LUKS2? (y/n
 read -r enroll_tpm2
 
 if [[ "$enroll_tpm2" == "y" ]]; then
-    echo -ne "${YELLOW}Type the path to the TPM2 device (Enter correctly, not contain spaces) (ex: /dev/tpm0): ${NC}"
-    read -r tpm2_device_path
-    echo -ne "${YELLOW}Type the disk path of the LUKS2 partition (Enter correctly, not contain spaces) (ex: /dev/nvme0n1p?): ${NC}"
-    read -r luks2_disk_path
-    echo "add_dracutmodules+=\" tpm2-tss \"" | sudo tee /etc/dracut.conf.d/tpm2.conf
-    systemd-cryptenroll --wipe-slot=tpm2 $luks2_disk_path
-    if systemd-detect-virt | grep -q "none"; then
-        systemd-cryptenroll --tpm2-device=$tpm2_device_path --tpm2-pcrs "7" $luks2_disk_path --tpm2-with-pin=yes
-    else
-        systemd-cryptenroll --tpm2-device=$tpm2_device_path --tpm2-pcrs "7" $luks2_disk_path
-    fi
-    dracut -f
-    echo -e "${GREEN}TPM2 device enrolled successfully.${NC}"
+	echo -ne "${YELLOW}Type the path to the TPM2 device (Enter correctly, not contain spaces) (ex: /dev/tpm0): ${NC}"
+	read -r tpm2_device_path
+	echo -ne "${YELLOW}Type the disk path of the LUKS2 partition (Enter correctly, not contain spaces) (ex: /dev/nvme0n1p?): ${NC}"
+	read -r luks2_disk_path
+	echo "add_dracutmodules+=\" tpm2-tss \"" | sudo tee /etc/dracut.conf.d/tpm2.conf
+	systemd-cryptenroll --wipe-slot=tpm2 $luks2_disk_path
+	if systemd-detect-virt | grep -q "none"; then
+		systemd-cryptenroll --tpm2-device=$tpm2_device_path --tpm2-pcrs=7 $luks2_disk_path # --tpm2-with-pin=yes
+	else
+		systemd-cryptenroll --tpm2-device=$tpm2_device_path --tpm2-pcrs "7" $luks2_disk_path
+	fi
+	dracut -f
+	echo -e "${GREEN}TPM2 device enrolled successfully.${NC}"
 fi
 
 dnf autoremove -y &>/dev/null
