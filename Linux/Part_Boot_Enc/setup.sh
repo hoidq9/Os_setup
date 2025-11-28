0ce16692ceca3fb2817507abbfb6bba0df536219
hoidq9 <hoidq9@gmail.com>
2025-11-28 11:37:13 +0700

    modify uuid partitions and check isLuks

--- Linux/Part_Boot_Enc/setup.sh
+++ Linux/Part_Boot_Enc/setup.sh
@@ -22,6 +22,21 @@ if [ ! -f /keys/key_luks2_tpm2_pcr/key.bin ]; then
 	chmod 600 /keys/key_luks2_tpm2_pcr/key_root.bin
 fi
 
+root_dev=$(findmnt -no SOURCE /)
+boot_dev=$(findmnt -no SOURCE /boot 2>/dev/null || true)
+
+if grep -q "/dev/mapper/" <<<"$boot_dev"; then
+	isLuksBoot=true
+else
+	isLuksBoot=false
+fi
+
+if grep -q "/dev/mapper/" <<<"$root_dev"; then
+	isLuksRoot=true
+else
+	isLuksRoot=false
+fi
+
 if [ "$os_id" = "rhel" ]; then
 	if ! rpm -q epel-release; then
 		dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-10.noarch.rpm -y # EPEL 10
@@ -46,7 +61,7 @@ if [ "$os_id" = "rhel" ]; then
 	unifont_otf
 
 elif [ "$os_id" = "fedora" ]; then
-	dnf install -y tpm2-tss-devel git json-c-devel util-linux pkg-config gcc make libfdisk-devel cryptsetup autoconf automake autopoint dejavu-sans-fonts dejavu-serif-fonts dejavu-sans-mono-fonts ranlib fuse3 fuse3-devel libtasn1-devel device-mapper-devel unifont unifont-fonts patch freetype-devel kernel-devel nss-tools pesign sbsigntools
+	dnf install -y tpm2-tss-devel git json-c-devel util-linux pkg-config gcc make libfdisk-devel cryptsetup autoconf automake autopoint dejavu-sans-fonts dejavu-serif-fonts dejavu-sans-mono-fonts ranlib fuse3 fuse3-devel libtasn1-devel device-mapper-devel unifont unifont-fonts patch freetype-devel kernel-devel nss-tools pesign sbsigntools libtool autoconf-archive
 fi
 
 dnf upgrade -y
@@ -134,87 +149,96 @@ create_one_time_file_enc_sha256() {
 
 grub2_bootloader_setup() {
 
-	build_grubx64_image() {
-		grep -q 'GRUB_FILE_TYPE_CRYPTODISK_ENCRYPTION_KEY' grub-core/kern/efi/sb.c || sed -i '/\*flags = GRUB_VERIFY_FLAGS_SKIP_VERIFICATION;/i\  case GRUB_FILE_TYPE_CRYPTODISK_ENCRYPTION_KEY:' grub-core/kern/efi/sb.c
-		./bootstrap
-		./autogen.sh
-		./configure --prefix="/repos/Grub2" --with-platform=efi --target=x86_64 --enable-stack-protector --enable-mm-debug --enable-cache-stats --enable-boot-time --enable-grub-emu-sdl2 --enable-grub-emu-sdl --enable-grub-emu-pci --enable-grub-mkfont --enable-grub-mount --enable-device-mapper --enable-liblzma --enable-grub-protect --with-gnu-ld --with-unifont=/usr/share/fonts/unifont/unifont.otf --with-dejavufont=/usr/share/fonts/dejavu-sans-fonts/DejaVuSans.ttf --enable-threads=posix+isoc --enable-cross-guesses=conservative --enable-dependency-tracking # --enable-libzfs --enable-grub-themes
-		make install
+	if [ "$isLuksBoot" == true ]; then
 
-		cp "$REPO_DIR"/config_grub.cfg /repos/Grub2/bin/config_"$os_id".cfg
-		cp "$REPO_DIR"/sbat_grub.csv /repos/Grub2/bin/sbat_"$os_id".csv
+		build_grubx64_image() {
+			grep -q 'GRUB_FILE_TYPE_CRYPTODISK_ENCRYPTION_KEY' grub-core/kern/efi/sb.c || sed -i '/\*flags = GRUB_VERIFY_FLAGS_SKIP_VERIFICATION;/i\  case GRUB_FILE_TYPE_CRYPTODISK_ENCRYPTION_KEY:' grub-core/kern/efi/sb.c
+			./bootstrap
+			./autogen.sh
+			./configure --prefix="/repos/Grub2" --with-platform=efi --target=x86_64 --enable-stack-protector --enable-mm-debug --enable-cache-stats --enable-boot-time --enable-grub-emu-sdl2 --enable-grub-emu-sdl --enable-grub-emu-pci --enable-grub-mkfont --enable-grub-mount --enable-device-mapper --enable-liblzma --enable-grub-protect --with-gnu-ld --with-unifont=/usr/share/fonts/unifont/unifont.otf --with-dejavufont=/usr/share/fonts/dejavu-sans-fonts/DejaVuSans.ttf --enable-threads=posix+isoc --enable-cross-guesses=conservative --enable-dependency-tracking # --enable-libzfs --enable-grub-themes
+			make install
 
-		grub_version_build=$(grep "grub_version" /repos/Grub2/lib/grub/x86_64-efi/modinfo.sh | cut -d'"' -f2)
-		sed -i "s/(version)/$grub_version_build/g" /repos/Grub2/bin/sbat_"$os_id".csv
-		sed -i "s/(efi_uuid)/$uuid_efi/g" /repos/Grub2/bin/config_"$os_id".cfg
-		sed -i "s/(boot_locked_uuid)/$uuid_boot_locked/g" /repos/Grub2/bin/config_"$os_id".cfg
-		sed -i "s/(boot_unlocked_uuid)/$uuid_boot_unlocked/g" /repos/Grub2/bin/config_"$os_id".cfg
+			cp "$REPO_DIR"/config_grub.cfg /repos/Grub2/bin/config_"$os_id".cfg
+			cp "$REPO_DIR"/sbat_grub.csv /repos/Grub2/bin/sbat_"$os_id".csv
 
-		cd /repos/Grub2/bin || return
-		./grub-mkimage -d ../lib/grub/x86_64-efi -p '' -o grubx64_new.efi -O x86_64-efi -c config_"$os_id".cfg -s sbat_"$os_id".csv at_keyboard boot keylayouts usbserial_common usb serial usbserial_usbdebug usbserial_ftdi usbserial_pl2303 tpm chain efinet net backtrace lsefimmap lsefi efifwsetup zstd xfs fshelp tftp test syslinuxcfg normal extcmd sleep terminfo search search_fs_uuid search_fs_file search_label regexp reboot png bitmap bufio pgp gcry_sha1 mpi crypto password_pbkdf2 pbkdf2 gcry_sha512 part_gpt part_msdos part_apple minicmd mdraid1x diskfilter mdraid09 luks2 afsplitter cryptodisk json luks lvm linux loopback jpeg iso9660 http halt acpi mmap gzio gcry_crc gfxmenu video font gfxterm bitmap_scale trig video_colors gcry_whirlpool gcry_twofish gcry_sha256 gcry_serpent gcry_rsa gcry_rijndael fat f2fs ext2 echo procfs archelp configfile cat loadenv disk gettext datetime terminal priority_queue all_video video_bochs video_cirrus efi_uga efi_gop video_fb probe btrfs afs bfs hfs zfs multiboot multiboot2 ls lsmmap ntfs smbios loadbios tpm2_key_protector usb_keyboard hashsum test
+			grub_version_build=$(grep "grub_version" /repos/Grub2/lib/grub/x86_64-efi/modinfo.sh | cut -d'"' -f2)
+			sed -i "s/(version)/$grub_version_build/g" /repos/Grub2/bin/sbat_"$os_id".csv
+			sed -i "s/(efi_uuid)/$uuid_efi/g" /repos/Grub2/bin/config_"$os_id".cfg
 
-		if [ "$os_id" == "fedora" ]; then
-			sbsign --key /keys/secureboot/"${os_id}"-"${user_current}".key --cert /keys/secureboot/"${os_id}"-"${user_current}".crt grubx64_new.efi --output grubx64.efi
-			cp grubx64.efi /boot/efi/EFI/fedora/
-		elif [ "$os_id" == "rhel" ]; then
-			pesign --in grubx64_new.efi --out grubx64.efi --certificate "${os_id}-${user_current}" --sign --force
-			cp grubx64.efi /boot/efi/EFI/redhat/
-		fi
-	}
+			boot_real=$(get_real_backing "$boot_dev")
+			uuid_boot_luks_locked=$(cryptsetup luksUUID "$boot_real")
+			mapped_name=$(basename "$boot_dev")
+			uuid_boot_luks_unlocked=$(blkid -s UUID -o value "/dev/mapper/$mapped_name")
 
-	mkdir -p /repos/Grub2
-	cd /repos || return
-	if [ -d grub2/.git ]; then
-		cd grub2 || return
-		BRANCH=$(git rev-parse --abbrev-ref HEAD)
-		git fetch origin "$BRANCH"
-		if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/"$BRANCH")" ] || [ ! -d gnulib ]; then
-			git pull
+			sed -i "s/(boot_locked_uuid)/$uuid_boot_luks_locked/g" /repos/Grub2/bin/config_"$os_id".cfg
+			sed -i "s/(boot_unlocked_uuid)/$uuid_boot_luks_unlocked/g" /repos/Grub2/bin/config_"$os_id".cfg
+
+			cd /repos/Grub2/bin || return
+			./grub-mkimage -d ../lib/grub/x86_64-efi -p '' -o grubx64_new.efi -O x86_64-efi -c config_"$os_id".cfg -s sbat_"$os_id".csv at_keyboard boot keylayouts usbserial_common usb serial usbserial_usbdebug usbserial_ftdi usbserial_pl2303 tpm chain efinet net backtrace lsefimmap lsefi efifwsetup zstd xfs fshelp tftp test syslinuxcfg normal extcmd sleep terminfo search search_fs_uuid search_fs_file search_label regexp reboot png bitmap bufio pgp gcry_sha1 mpi crypto password_pbkdf2 pbkdf2 gcry_sha512 part_gpt part_msdos part_apple minicmd mdraid1x diskfilter mdraid09 luks2 afsplitter cryptodisk json luks lvm linux loopback jpeg iso9660 http halt acpi mmap gzio gcry_crc gfxmenu video font gfxterm bitmap_scale trig video_colors gcry_whirlpool gcry_twofish gcry_sha256 gcry_serpent gcry_rsa gcry_rijndael fat f2fs ext2 echo procfs archelp configfile cat loadenv disk gettext datetime terminal priority_queue all_video video_bochs video_cirrus efi_uga efi_gop video_fb probe btrfs afs bfs hfs zfs multiboot multiboot2 ls lsmmap ntfs smbios loadbios tpm2_key_protector usb_keyboard hashsum test
+
+			if [ "$os_id" == "fedora" ]; then
+				sbsign --key /keys/secureboot/"${os_id}"-"${user_current}".key --cert /keys/secureboot/"${os_id}"-"${user_current}".crt grubx64_new.efi --output grubx64.efi
+				cp grubx64.efi /boot/efi/EFI/fedora/
+			elif [ "$os_id" == "rhel" ]; then
+				pesign --in grubx64_new.efi --out grubx64.efi --certificate "${os_id}-${user_current}" --sign --force
+				cp grubx64.efi /boot/efi/EFI/redhat/
+			fi
+		}
+
+		mkdir -p /repos/Grub2
+		cd /repos || return
+		if [ -d grub2/.git ]; then
+			cd grub2 || return
+			BRANCH=$(git rev-parse --abbrev-ref HEAD)
+			git fetch origin "$BRANCH"
+			if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/"$BRANCH")" ] || [ ! -d gnulib ]; then
+				git pull
+				build_grubx64_image
+			fi
+		else
+			git clone -b master https://github.com/rhboot/grub2.git
+			cd grub2 || return
 			build_grubx64_image
 		fi
-	else
-		git clone -b master https://github.com/rhboot/grub2.git
-		cd grub2 || return
-		build_grubx64_image
 	fi
-
 }
 
 pcr_oracle_tpm2_seal() {
 
-	build_sealed_tpm() {
-		./configure
-		make install
+	if [ "$isLuksBoot" == true ]; then
+		build_sealed_tpm() {
+			./configure
+			make install
 
-		cd /keys/key_luks2_tpm2_pcr || return
+			cd /keys/key_luks2_tpm2_pcr || return
 
-		pcr-oracle --rsa-generate-key --private-key my-priv.pem --authorized-policy my-auth.policy create-authorized-policy 0,4,7,14
+			pcr-oracle --rsa-generate-key --private-key my-priv.pem --authorized-policy my-auth.policy create-authorized-policy 0,4,7,14
 
-		pcr-oracle --target-platform tpm2.0 --authorized-policy my-auth.policy --input key.bin --output unsigned.tpm seal-secret
+			pcr-oracle --target-platform tpm2.0 --authorized-policy my-auth.policy --input key.bin --output unsigned.tpm seal-secret
 
-		pcr-oracle --policy-name authorized-policy-test --input unsigned.tpm --output sealed.tpm --target-platform tpm2.0 --algorithm sha256 --private-key my-priv.pem --from eventlog --stop-event "grub-file=grub.cfg" --before sign 0,4,7,14
+			pcr-oracle --policy-name authorized-policy-test --input unsigned.tpm --output sealed.tpm --target-platform tpm2.0 --algorithm sha256 --private-key my-priv.pem --from eventlog --stop-event "grub-file=grub.cfg" --before sign 0,4,7,14
 
-		cp sealed.tpm /boot/efi/
+			cp sealed.tpm /boot/efi/
 
-	}
+		}
 
-	mkdir -p /repos
-	cd /repos || return
-	if [ -d pcr-oracle/.git ]; then
-		cd pcr-oracle || return
-		BRANCH=$(git rev-parse --abbrev-ref HEAD)
-		git fetch origin "$BRANCH"
-		if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/"$BRANCH")" ]; then
-			git pull
+		mkdir -p /repos
+		cd /repos || return
+		if [ -d pcr-oracle/.git ]; then
+			cd pcr-oracle || return
+			BRANCH=$(git rev-parse --abbrev-ref HEAD)
+			git fetch origin "$BRANCH"
+			if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/"$BRANCH")" ]; then
+				git pull
+				build_sealed_tpm
+			fi
+
+		else
+			git clone https://github.com/openSUSE/pcr-oracle.git
+			cd pcr-oracle || return
 			build_sealed_tpm
 		fi
-
-	else
-		git clone https://github.com/openSUSE/pcr-oracle.git
-		cd pcr-oracle || return
-		build_sealed_tpm
 	fi
-
 }
 
 create_keys_secureboot() {
@@ -317,22 +341,7 @@ create_luks2_boot_partition() {
 	# cryptsetup isLuks "$dev" >/dev/null 2>&1
 	# result=$?
 
-	root_dev=$(findmnt -no SOURCE /)
-	boot_dev=$(findmnt -no SOURCE /boot 2>/dev/null || true)
-
-	if grep -q "/dev/mapper/" <<<"$boot_dev"; then
-		isLuks=true
-	else
-		isLuks=false
-	fi
-
-	if grep -q "/dev/mapper/" <<<"$root_dev"; then
-		isLuksRoot=true
-	else
-		isLuksRoot=false
-	fi
-
-	if [[ "$boot_dev" != "$root_dev" ]] && [ "$isLuks" == false ] && [ "$isLuksRoot" == true ]; then
+	if [[ "$boot_dev" != "$root_dev" ]] && [ "$isLuksBoot" == false ] && [ "$isLuksRoot" == true ]; then
 
 		cd / || return
 
@@ -350,8 +359,8 @@ create_luks2_boot_partition() {
 		root_real=$(get_real_backing "$root_dev")
 
 		echo -e "${GREEN} Action with device ${dev} ${NC}"
-		cryptsetup luksFormat --uuid="$uuid_boot_locked" --hash=sha256 --key-size=512 --label=LinuxH --pbkdf=pbkdf2 --pbkdf-force-iterations=3986521 --use-urandom "$dev"
-		cryptsetup luksAddKey "$dev" /keys/key_luks2_tpm2_pcr/key.bin --pbkdf=pbkdf2 --pbkdf-force-iterations=3986521
+		cryptsetup luksFormat --uuid="$uuid_boot_locked" --hash=sha256 --key-size=512 --label=LinuxH --pbkdf=pbkdf2 --use-urandom "$dev"
+		cryptsetup luksAddKey "$dev" /keys/key_luks2_tpm2_pcr/key.bin --pbkdf=pbkdf2
 
 		echo -e "${GREEN} Action with device ${root_real} ${NC}"
 		cryptsetup luksAddKey "$root_real" /keys/key_luks2_tpm2_pcr/key_root.bin
@@ -359,9 +368,9 @@ create_luks2_boot_partition() {
 		systemd-cryptsetup attach my_crypt "$dev" /keys/key_luks2_tpm2_pcr/key.bin
 
 		if [ "$fsType" == "xfs" ]; then
-			mkfs.xfs -m uuid="$uuid_boot_locked" -L LinuxH /dev/mapper/my_crypt
+			mkfs.xfs -m uuid="$uuid_boot_unlocked" -L LinuxH /dev/mapper/my_crypt
 		elif [ "$fsType" == "ext4" ]; then
-			mkfs.ext4 -U "$uuid_boot_locked" -L LinuxH /dev/mapper/my_crypt
+			mkfs.ext4 -U "$uuid_boot_unlocked" -L LinuxH /dev/mapper/my_crypt
 		fi
 
 		sed -i "s/$uuid_old/$uuid_boot_unlocked/g" /etc/fstab
@@ -422,9 +431,9 @@ create_luks2_boot_partition() {
 # main() {
 # create_one_time_file_enc_sha256
 create_keys_secureboot
+create_luks2_boot_partition
 grub2_bootloader_setup
 pcr_oracle_tpm2_seal
-create_luks2_boot_partition
 # }
 
 # main |& tee /result.txt
