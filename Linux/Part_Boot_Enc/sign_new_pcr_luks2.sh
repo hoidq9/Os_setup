3f1f1b801b4cf20d99053f17024174e78d17fff6
hoidq9 <hoidq9@gmail.com>
2025-12-23 17:37:15 +0700

    add script sign new sealed.tpm when tpm2 pcr change

---
+++ Linux/Part_Boot_Enc/sign_new_pcr_luks2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /keys/key_luks2_tpm2_pcr || return
+
+pcr-oracle --rsa-generate-key --private-key my-priv.pem --authorized-policy my-auth.policy create-authorized-policy 0,4,7,14
+
+pcr-oracle --target-platform tpm2.0 --authorized-policy my-auth.policy --input key.bin --output unsigned.tpm seal-secret
+
+pcr-oracle --policy-name authorized-policy-test --input unsigned.tpm --output sealed.tpm --target-platform tpm2.0 --algorithm sha256 --private-key my-priv.pem --from eventlog --stop-event "grub-file=grub.cfg" --before sign 0,4,7,14
+
+cp sealed.tpm /boot/efi
