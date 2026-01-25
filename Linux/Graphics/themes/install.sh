60c27127082fd65dd1be7a7a357df191fb7bb177
hoidq9 <hoidq9@gmail.com>
2026-01-25 18:23:33 +0700

    disable gnome shell themes

--- Linux/Graphics/themes/install.sh
+++ Linux/Graphics/themes/install.sh
@@ -2,49 +2,49 @@
 source ../../variables.sh
 
 dependencies() {
-    cd "$REPO_DIR" || return
-    mkdir -p themes_os
-    cd themes_os
-    mkdir -p WhiteSur-gtk-theme && curl -L $(curl -s https://api.github.com/repos/vinceliuice/WhiteSur-gtk-theme/releases/latest | grep "tarball" | cut -d '"' -f 4) | tar -xz -C WhiteSur-gtk-theme --strip-components=1
-    cd WhiteSur-gtk-theme
+	cd "$REPO_DIR" || return
+	mkdir -p themes_os
+	cd themes_os
+	mkdir -p WhiteSur-gtk-theme && curl -L $(curl -s https://api.github.com/repos/vinceliuice/WhiteSur-gtk-theme/releases/latest | grep "tarball" | cut -d '"' -f 4) | tar -xz -C WhiteSur-gtk-theme --strip-components=1
+	cd WhiteSur-gtk-theme
 }
 
 os_themes() {
-    dependencies
-    local os_name="$1"
+	dependencies
+	local os_name="$1"
 
-    if [ "$EUID" -eq 0 ]; then
-        rm -rf src/assets/gnome-shell/backgrounds/*
-        for target in background-default.png background-darken.png background-blank.png background-blur-darken.png background-blur.png; do
-            cp $REPO_DIR/Lenovo_Legion.png src/assets/gnome-shell/backgrounds/$target
-        done
-        gawk -i inplace '!/Yaru/' src/main/gnome-shell/gnome-shell-theme.gresource.xml
-        ./install.sh -n 'WoW' -o normal -c light -a alt -t default -s standard -m -N mojave -HD --round --shell -b default -p 30 -h bigger -normal -sf
-        ./tweaks.sh -o solid -c light -t default -s standard -g -b default -p 30 -h bigger -sf
-        cd /usr/share/themes/
+	if [ "$EUID" -eq 0 ]; then
+		rm -rf src/assets/gnome-shell/backgrounds/*
+		for target in background-default.png background-darken.png background-blank.png background-blur-darken.png background-blur.png; do
+			cp $REPO_DIR/Lenovo_Legion.png src/assets/gnome-shell/backgrounds/$target
+		done
+		gawk -i inplace '!/Yaru/' src/main/gnome-shell/gnome-shell-theme.gresource.xml
+		./install.sh -n 'WoW' -o normal -c light -a alt -t default -s standard -m -N mojave -HD --round --shell -b default -p 30 -h bigger -normal -sf
+		./tweaks.sh -o solid -c light -t default -s standard -g -b default -p 30 -h bigger -sf
+		cd /usr/share/themes/
 
-        if [ ! -z "$(ls -A WoW-Light-alt)" ]; then
-            rm -rf "${os_name}_themes"
-            mv WoW-Light-alt "${os_name}_themes"
-            cd "${os_name}_themes" || return
-            rm -rf cinnamon plank # gnome-shell
-            cd ..
-            rm -rf WoW*
-        fi
+		if [ ! -z "$(ls -A WoW-Light-alt)" ]; then
+			rm -rf "${os_name}_themes"
+			mv WoW-Light-alt "${os_name}_themes"
+			cd "${os_name}_themes" || return
+			rm -rf cinnamon plank # gnome-shell
+			cd ..
+			rm -rf WoW*
+		fi
 
-    else
-        if [ ! -d "/usr/share/themes/${os_name}_themes" ]; then
-            mkdir -p "$HOME/.local/share/themes/${os_name}_themes"
-            cd release/
-            tar -xJf WhiteSur-Light.tar.xz -C $HOME/.local/share/themes/${os_name}_themes --strip-components=1
-        fi
-    fi
-    cd "$REPO_DIR" || return
-    rm -rf themes_os
+	else
+		if [ ! -d "/usr/share/themes/${os_name}_themes" ]; then
+			mkdir -p "$HOME/.local/share/themes/${os_name}_themes"
+			cd release/
+			tar -xJf WhiteSur-Light.tar.xz -C $HOME/.local/share/themes/${os_name}_themes --strip-components=1
+		fi
+	fi
+	cd "$REPO_DIR" || return
+	rm -rf themes_os
 }
 
 Graphics_themes() {
-    os_themes "$os_id"
+	os_themes "$os_id"
 }
 
-check_and_run Graphics_themes "$REPO_DIR/../../logs/Graphics_themes.log" "$REPO_DIR/../../logs/Result.log"
+# check_and_run Graphics_themes "$REPO_DIR/../../logs/Graphics_themes.log" "$REPO_DIR/../../logs/Result.log"
