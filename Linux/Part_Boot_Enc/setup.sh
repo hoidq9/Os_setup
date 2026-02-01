a62da96d38092884301360a0e5529581c5eece72
hoidq9 <hoidq9@gmail.com>
2026-02-01 08:20:42 +0700

    update

--- Linux/Part_Boot_Enc/setup.sh
+++ Linux/Part_Boot_Enc/setup.sh
@@ -223,11 +223,11 @@ pcr_oracle_tpm2_seal() {
 
 			cd /keys/key_luks2_tpm2_pcr || return
 
-			pcr-oracle --rsa-generate-key --private-key my-priv.pem --authorized-policy my-auth.policy create-authorized-policy 0,1,4,7,14
+			pcr-oracle --rsa-generate-key --private-key my-priv.pem --authorized-policy my-auth.policy create-authorized-policy 0,4,7,14
 
 			pcr-oracle --target-platform tpm2.0 --authorized-policy my-auth.policy --input key.bin --output unsigned.tpm seal-secret
 
-			pcr-oracle --policy-name authorized-policy-test --input unsigned.tpm --output sealed.tpm --target-platform tpm2.0 --algorithm sha256 --private-key my-priv.pem --from eventlog --stop-event "grub-file=grub.cfg" --before sign 0,1,4,7,14
+			pcr-oracle --policy-name authorized-policy-test --input unsigned.tpm --output sealed.tpm --target-platform tpm2.0 --algorithm sha256 --private-key my-priv.pem --from eventlog --stop-event "grub-file=grub.cfg" --before sign 0,4,7,14
 
 			if mountpoint -q /boot/efi; then
 				cp sealed.tpm /boot/efi
