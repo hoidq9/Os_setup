f6d34000ecf550f4cb62791b6a38ee98b1fa6dd6
hoidq9 <hoidq9@gmail.com>
2026-03-10 16:24:52 +0700

    update

---
+++ Linux/Part_Boot_Enc/build_uki/1.sh
@@ -0,0 +1,4 @@
+openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -out /etc/systemd/tpm2-pcr-private-key.pem
+openssl rsa -pubout -in /etc/systemd/tpm2-pcr-private-key.pem -out /etc/systemd/tpm2-pcr-public-key.pem
+
+ukify -c /etc/ukify.cfg build --output /boot/ukify-linux.efi
