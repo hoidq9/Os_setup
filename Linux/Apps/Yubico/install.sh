72126b52ebc3c60a3171c857474f3f50777fc7c6
hoidq9 <hoidq9@gmail.com>
2026-04-02 17:13:28 +0700

    config app yubico

--- Linux/Apps/Yubico/install.sh
+++ Linux/Apps/Yubico/install.sh
@@ -2,21 +2,26 @@
 source ../../variables.sh
 
 Apps_Yubico() {
-    if [ ! -d "$HOME/Prj/Yubico" ]; then
-        mkdir -p $HOME/Prj/Yubico
-    fi
-    cd "$HOME/Prj/Yubico" || return
-    rm -rf *
-    # wget $(curl -s https://api.github.com/repos/Yubico/yubioath-flutter/releases/latest | grep "browser_download_url" | grep "linux" | grep "tar.gz" | cut -d '"' -f 4)
-    wget https://developers.yubico.com/yubioath-flutter/Releases/yubico-authenticator-latest-linux.tar.gz    
-    yubico_compress=$(ls -d *.tar.gz)
-    dir_name=$(basename $yubico_compress .tar.gz)
-    mkdir -p $dir_name
-    tar -xzvf $yubico_compress -C $dir_name --strip-components=1
-    cd "$dir_name" || return
-    ./desktop_integration.sh -i
-    cd "$HOME/Prj/Yubico" || return
-    rm -rf *.tar.gz *.tar.gz.sig .tar.gz
+	if [ ! -d "$HOME/Prj/Yubico" ]; then
+		mkdir -p $HOME/Prj/Yubico
+	fi
+	cd "$HOME/Prj/Yubico" || return
+	rm -rf *
+	# wget $(curl -s https://api.github.com/repos/Yubico/yubioath-flutter/releases/latest | grep "browser_download_url" | grep "linux" | grep "tar.gz" | cut -d '"' -f 4)
+	wget https://developers.yubico.com/yubioath-flutter/Releases/yubico-authenticator-latest-linux.tar.gz
+	yubico_compress=$(ls -d *.tar.gz)
+	dir_name=$(basename $yubico_compress .tar.gz)
+	mkdir -p $dir_name
+	tar -xzvf $yubico_compress -C $dir_name --strip-components=1
+	cd "$dir_name" || return
+
+	# if yubico desktop file not exist, install
+	if [ ! -f "$HOME/.local/share/applications/com.yubico.yubioath.desktop" ]; then
+		./desktop_integration.sh -i
+	fi
+
+	cd "$HOME/Prj/Yubico" || return
+	rm -rf *.tar.gz *.tar.gz.sig .tar.gz
 }
 
 check_and_run Apps_Yubico "$REPO_DIR/../../logs/Apps_Yubico.log" "$REPO_DIR/../../logs/Result.log"
