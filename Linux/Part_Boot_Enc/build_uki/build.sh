3560a97365ac917bfc985b8914c46dfe66c4eb7f
hoidq9 <hoidq9@gmail.com>
2026-04-08 12:36:27 +0700

    update kernel paras and plymouth config

--- Linux/Part_Boot_Enc/build_uki/build.sh
+++ Linux/Part_Boot_Enc/build_uki/build.sh
@@ -24,7 +24,8 @@ dnf install systemd-ukify -y &>/dev/null 2>&1
 
 vmlinuz_path="/usr/lib/modules/${latest_kernel}/vmlinuz"
 initramfs_path="/boot/initramfs-${latest_kernel}.img"
-cmdline_path="${parameters} rhgb lockdown=confidentiality intel_iommu=on efi=disable_early_pci_dma module.sig_enforce=1 slab_nomerge page_alloc.shuffle=1 pti=on spectre_v2=on spec_store_bypass_disable=on vsyscall=none randomize_kstack_offset=on random.trust_cpu=on"
+cmdline_path="${parameters} rhgb lockdown=confidentiality intel_iommu=on efi=disable_early_pci_dma module.sig_enforce=1 slab_nomerge page_alloc.shuffle=1 pti=on spectre_v2=on spec_store_bypass_disable=on vsyscall=none randomize_kstack_offset=on random.trust_cpu=on rd.systemd.show_status=1 loglevel=7 plymouth.enable=1 rd.plymouth=1"
+
 private_key_path="/keys/secureboot/${os_id}-${user_current}.key"
 certificate_path="/keys/secureboot/${os_id}-${user_current}.x509"
 
@@ -34,6 +35,7 @@ sed -i "s|(initramfs)|${initramfs_path}|g" setup.cfg
 sed -i "s|(cmdline)|${cmdline_path}|g" setup.cfg
 sed -i "s|(private-key)|${private_key_path}|g" setup.cfg
 sed -i "s|(certificate)|${certificate_path}|g" setup.cfg
+[[ "$(plymouth-set-default-theme)" != "details" ]] && plymouth-set-default-theme details -R || echo "Not changed."
 
 dracut -f -v --regenerate-all
 ukify build --config=${REPO_DIR}/setup.cfg --output /boot/ukify-linux.efi
