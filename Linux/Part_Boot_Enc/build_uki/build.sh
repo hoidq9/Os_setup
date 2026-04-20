f83a656eceb1b29bafc9862b58907e6ae05ffd25
hoidq9 <hoidq9@gmail.com>
2026-04-20 21:14:33 +0700

    change length key RSA

--- Linux/Part_Boot_Enc/build_uki/build.sh
+++ Linux/Part_Boot_Enc/build_uki/build.sh
@@ -16,11 +16,11 @@ fi
 ./create_keys_secureboot.sh
 
 if [ ! -f /keys/secureboot/tpm2-pcr-private-key.pem ]; then
-	openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -out /keys/secureboot/tpm2-pcr-private-key.pem
+	openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 -out /keys/secureboot/tpm2-pcr-private-key.pem
 	openssl rsa -pubout -in /keys/secureboot/tpm2-pcr-private-key.pem -out /etc/systemd/tpm2-pcr-public-key.pem
 fi
 
-dnf install systemd-ukify -y &>/dev/null 2>&1
+dnf install systemd-ukify -y # /&>/dev/null 2>&1
 
 vmlinuz_path="/usr/lib/modules/${latest_kernel}/vmlinuz"
 initramfs_path="/boot/initramfs-${latest_kernel}.img"
