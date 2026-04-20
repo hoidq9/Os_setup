993e4b40ac9ba10cc4ea003be23176b873c9b0e8
hoidq9 <hoidq9@gmail.com>
2026-04-20 10:26:55 +0700

    Install from source flathub

--- Linux/Apps/Yubico/install.sh
+++ Linux/Apps/Yubico/install.sh
@@ -2,26 +2,28 @@
 source ../../variables.sh
 
 Apps_Yubico() {
-	if [ ! -d "$HOME/Prj/Yubico" ]; then
-		mkdir -p $HOME/Prj/Yubico
-	fi
-	cd "$HOME/Prj/Yubico" || return
-	rm -rf *
-	# wget $(curl -s https://api.github.com/repos/Yubico/yubioath-flutter/releases/latest | grep "browser_download_url" | grep "linux" | grep "tar.gz" | cut -d '"' -f 4)
-	wget https://developers.yubico.com/yubioath-flutter/Releases/yubico-authenticator-latest-linux.tar.gz
-	yubico_compress=$(ls -d *.tar.gz)
-	dir_name=$(basename $yubico_compress .tar.gz)
-	mkdir -p $dir_name
-	tar -xzvf $yubico_compress -C $dir_name --strip-components=1
-	cd "$dir_name" || return
+	# if [ ! -d "$HOME/Prj/Yubico" ]; then
+	# 	mkdir -p $HOME/Prj/Yubico
+	# fi
+	# cd "$HOME/Prj/Yubico" || return
+	# rm -rf *
+	# # wget $(curl -s https://api.github.com/repos/Yubico/yubioath-flutter/releases/latest | grep "browser_download_url" | grep "linux" | grep "tar.gz" | cut -d '"' -f 4)
+	# wget https://developers.yubico.com/yubioath-flutter/Releases/yubico-authenticator-latest-linux.tar.gz
+	# yubico_compress=$(ls -d *.tar.gz)
+	# dir_name=$(basename $yubico_compress .tar.gz)
+	# mkdir -p $dir_name
+	# tar -xzvf $yubico_compress -C $dir_name --strip-components=1
+	# cd "$dir_name" || return
 
-	# if yubico desktop file not exist, install
-	if [ ! -f "$HOME/.local/share/applications/com.yubico.yubioath.desktop" ]; then
-		./desktop_integration.sh -i
-	fi
+	# # if yubico desktop file not exist, install
+	# if [ ! -f "$HOME/.local/share/applications/com.yubico.yubioath.desktop" ]; then
+	# 	./desktop_integration.sh -i
+	# fi
 
-	cd "$HOME/Prj/Yubico" || return
-	rm -rf *.tar.gz *.tar.gz.sig .tar.gz
+	# cd "$HOME/Prj/Yubico" || return
+	# rm -rf *.tar.gz *.tar.gz.sig .tar.gz
+
+	flatpak install flathub com.yubico.yubioath -y
 }
 
 check_and_run Apps_Yubico "$REPO_DIR/../../logs/Apps_Yubico.log" "$REPO_DIR/../../logs/Result.log"
