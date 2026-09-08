9dbe01b7394df76685373724662468902e7d9a33
hoidq9 <hoidq9@gmail.com>
2026-09-08 19:10:36 +0700

    update argos and disable extension Burn My Windows for gnome fedora

--- Linux/User/setup.sh
+++ Linux/User/setup.sh
@@ -249,7 +249,7 @@ User_setup() {
 		# wget -N -q "https://raw.githubusercontent.com/ToasterUwU/install-gnome-extensions/master/install-gnome-extensions.sh" -O ./install-gnome-extensions.sh
 
 		if [ "$os_id" == "fedora" ]; then
-			extensions=('3628' '2087' '6682' '4679' '1160' '3843' '3010' '3733' '6580' '2932')
+			extensions=('3628' '2087' '6682' '1160' '3843' '3010' '3733' '6580' '2932') # '4679'
 		elif [ "$os_id" == "rhel" ]; then
 			#     extensions=('1486' '3088' '3628' '4679' '1082' '3843' '120' '3733' '5219' '1460' '4670' '1160' '6272')
 			# if systemd-detect-virt | grep -q "none"; then
@@ -383,9 +383,11 @@ User_setup() {
 				git pull
 			fi
 
-			cp -rf $HOME/Prj/argos/argos@pew.worldwidemann.com ~/.local/share/gnome-shell/extensions/
+			mv -f $HOME/Prj/argos/argos@pew.worldwidemann.com ~/.local/share/gnome-shell/extensions/
 		fi
 
+		cd $HOME/Prj
+		rm -rf $HOME/Prj/argos
 		mkdir -p $HOME/.config/argos
 		rm -rf $HOME/.config/argos/*.sh
 		cd $REPO_DIR/extensions_gnome/argos
