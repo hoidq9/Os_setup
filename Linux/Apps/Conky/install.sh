85dcf28578c548cd475bbc091a6a0868990b80fc
hoidq9 <hoidq9@gmail.com>
2026-09-08 19:13:43 +0700

    disable conky fedora

--- Linux/Apps/Conky/install.sh
+++ Linux/Apps/Conky/install.sh
@@ -101,6 +101,7 @@ Apps_Conky() {
 
 if systemd-detect-virt | grep -q "none"; then
 	if [ "$os_id" == "fedora" ]; then
-		check_and_run Apps_Conky "$REPO_DIR/../../logs/Apps_Conky.log" "$REPO_DIR/../../logs/Result.log"
+		# check_and_run Apps_Conky "$REPO_DIR/../../logs/Apps_Conky.log" "$REPO_DIR/../../logs/Result.log"
+		echo "hehe"
 	fi
 fi
