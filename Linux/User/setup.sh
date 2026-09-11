3cf6f6ffa098c725038566a028cc1689ea72cd0e
hoidq9 <hoidq9@gmail.com>
2026-09-11 07:42:12 +0700

    install input method or extensions depend on desktop environment

--- Linux/User/setup.sh
+++ Linux/User/setup.sh
@@ -432,11 +432,11 @@ User_setup() {
 
 	podman_service
 	check_and_run Ohmyzsh_User "$REPO_DIR/../logs/Ohmyzsh_User.log" "$REPO_DIR/../logs/Result.log"
-	if systemd-detect-virt | grep -q "none" && [ $(cat ../DE.txt) == "GNOME" ]; then
+	if systemd-detect-virt | grep -q "none" && [ $(echo $XDG_CURRENT_DESKTOP) == "GNOME" ]; then
 		check_and_run User_gnome_extensions "$REPO_DIR/../logs/User_gnome_extensions.log" "$REPO_DIR/../logs/Result.log"
 	fi
 
-	if [ $(cat $REPO_DIR/../DE.txt) == "KDE" ]; then
+	if [ $(echo $XDG_CURRENT_DESKTOP) == "KDE" ]; then
 		# kde_keyring
 		KDE_Desktop
 	fi
