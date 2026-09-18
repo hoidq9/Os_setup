f3b025109fb98a4e5fa957ebcfc22b2248555c0e
hoidq9 <hoidq9@gmail.com>
2026-09-18 16:22:12 +0700

    remove comment

--- Linux/variables.sh
+++ Linux/variables.sh
@@ -6,7 +6,7 @@ os_id=$(awk -F= '/^ID=/{gsub(/"/, "", $2); print $2}' /etc/os-release)
 YELLOW='\033[1;33m'
 GREEN='\033[1;32m'
 RED='\033[0;31m'
-NC='\033[0m' # Không màu
+NC='\033[0m'
 
 check_and_run() {
 	local task_name="$1"
@@ -16,7 +16,7 @@ check_and_run() {
 		echo ""
 		echo "Task $task_name: Already completed, skipping."
 	else
-		echo "" # Print an empty line for spacing
+		echo ""
 		echo -e "${YELLOW}Task $task_name: Start${NC}"
 		sleep 3
 		echo -e "\033[A\033[K${RED}Task $task_name: Running${NC}"
